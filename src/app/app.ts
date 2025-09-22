aa3be629d0152b5e31503f14be33cfee7395c6a0
StefanoR <rubinetti.stefano@gmail.com>
2025-09-22 15:21:48 +0200

    aggiunto http e proxy

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('TemplateHttp');
 }
